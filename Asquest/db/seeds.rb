b0435e71a449f84c52f7d0f8231910da43864b97
u_ta <yu.business25@gmail.com>
2019-04-17 22:44:19 +0900

    first commit

---
+++ Asquest/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Question.create(id: 1, name: 'Test name 1', title: 'Test title 1', content: 'test content 1')
+Question.create(id: 2, name: 'Test name 2', title: 'Test title 2', content: 'test content 2')
+Question.create(id: 3, name: 'Test name 3', title: 'Test title 3', content: 'test content 3')
