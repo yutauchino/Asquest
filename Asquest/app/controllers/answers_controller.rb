66ad7a0b36536cf7638a46f5439e3455a688a3f5
u_ta <yu.business25@gmail.com>
2019-04-20 20:11:36 +0900

    回答の編集機能を実装

--- Asquest/app/controllers/answers_controller.rb
+++ Asquest/app/controllers/answers_controller.rb
@@ -10,9 +10,22 @@ class AnswersController < ApplicationController
     end
 
   end
+
   def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
   end
 
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.update(answer_params)
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      flash[:alert] = 'Invaild!'
+      render 'Edit'
+    end
+  end
   private
     def answer_params
       params.require(:answer).permit(:content, :name, :question_id)
