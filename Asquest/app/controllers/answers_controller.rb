d8e59abdf5cb3485d002486de534c402d12f71b5
u_ta <yu.business25@gmail.com>
2019-04-20 19:51:17 +0900

    質問回答内容を質問詳細画面に表示するように実装

--- Asquest/app/controllers/answers_controller.rb
+++ Asquest/app/controllers/answers_controller.rb
@@ -1,4 +1,20 @@
 class AnswersController < ApplicationController
+  
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+    if @answer.update(answer_params)
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      redirect_to question_path(@question), alert: 'Invaild!'
+    end
+
+  end
   def edit
   end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:content, :name, :question_id)
+    end
 end
